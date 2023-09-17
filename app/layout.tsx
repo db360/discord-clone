e558f27bf8a4da2e9a52e5ce11bbe4726b86cbfe
David Martinez <da.b@hotmail.es>
2023-09-17 22:14:13 +0200

    socketio connection, chat-header socket.io indicat

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -20,7 +22,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
@@ -31,8 +32,10 @@ export default function RootLayout({
             enableSystem={true}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
